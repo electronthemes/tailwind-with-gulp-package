f6c5e9cf983e3464b6d0695cdcfeb3ca719e22d4
Prantho <pranthodey89@gmail.com>
2023-01-04 19:08:14 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: 'jit', // Just-In-Time Compiler
+  purge: ['./src/**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    // Font Family
+    // fontFamily: {
+    //   Josefin: ['Josefin Sans', 'sans-serif'],
+    //   julyIt: ['July it'],
+    // },
+
+    extend: {
+      // Color
+      colors: {
+        'primary': '#BB976D',
+        'secondary': '#E13939',
+        'tertiary': '#85A3B4',
+      },
+      maxWidth: {
+        '1366': '1366px',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
+}
+
